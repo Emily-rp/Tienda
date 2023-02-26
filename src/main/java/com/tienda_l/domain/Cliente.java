675bbc05b751a298b4b4afa72247d5fe44a05d23
Emily <emily.rp@hotmail.com>
2023-02-26 07:22:17 -0600

    Revert "MySQL"
    This reverts commit 6a1c157e965deeb8cb57cf63df2956a51fd6469d.

--- src/main/java/com/tienda_l/domain/Cliente.java
+++ src/main/java/com/tienda_l/domain/Cliente.java
@@ -1,22 +1,11 @@
 
 package com.tienda_l.domain;
 
-import jakarta.persistence.*;//asterisco*
-import java.io.Serializable;//persistencia en objetos
 import lombok.Data;
 
 @Data//genera get, set equals ...
-@Entity //va mapiar una tabla de la base de datos
-@Table(name="cliente")
 
-public class Cliente implements Serializable {
-    
-    private static final long serialVersionUID = 1L;
-    
-    @Id //el atributo que sigue es linea primaria
-    @GeneratedValue(strategy = GenerationType.IDENTITY)//vuando genere un valor use el que genera en base de datos
-    private Long idCliente;
-   
+public class Cliente {
     private String nombre;
     private String apellidos;
     private String correo;
